d512ba6ac691213fe112138e001846cf6425fca1
kimusann0604 <fantianmucun@gmail.com>
2024-09-30 00:26:16 +0900

    🛠️データベースを削除

--- db.php
+++ db.php
@@ -1,11 +1,9 @@
 <?php
 try {
-    // PDOでデータベースに接続
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=test;port=8889;charset=utf8', 'root', 'root');
-    // エラーモードを例外に設定
+
+    $pdo = new PDO('mysql:host='ホスト名';dbname='データベース名';port='port選択';charset=utf8', 'root', 'root');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // エラーが発生した場合、エラーメッセージを表示して終了
     echo 'データベース接続に失敗しました: ' . $e->getMessage();
     exit;
 }
